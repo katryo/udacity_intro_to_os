f89fe75519996247a8ed5c8ef6ad9eed8d8d4faf
Ryo Kato <katoryo55@gmail.com>
2017-07-04 13:43:53 +0900

    Change the quiz

--- pthreadquiz1.c
+++ pthreadquiz1.c
@@ -1,28 +1,25 @@
-/* A Slightly Less Simple PThreads Example */
+/* PThread Creation Quiz 1 */
 
 #include <stdio.h>
 #include <pthread.h>
 
 #define NUM_THREADS 4
 
-void *threadFunc(void *pArg) { /* thread main */
-    int *p = (int*)pArg;
-    int myNum = *p;
-    printf("Thread number %d\n", myNum);
-    return 0;
+void *hello (void *arg) { /* thread main */
+	printf("Hello Thread\n");
+	return 0;
 }
 
-int main(void) {
-    int i;
-    pthread_t tid[NUM_THREADS];
-    
-    for(i = 0; i < NUM_THREADS; i++) { /* create/fork threads */
-        pthread_create(&tid[i], NULL, threadFunc, &i);
-    }
-    
-    for(i = 0; i < NUM_THREADS; i++) { /* wait/join threads */
-        pthread_join(tid[i], NULL);
-    }
-    return 0;
-}
+int main (void) {
+	int i;
+	pthread_t tid[NUM_THREADS];
 
+	for (i = 0; i < NUM_THREADS; i++) { /* create/fork threads */
+		pthread_create(&tid[i], NULL, hello, NULL);
+	}
+
+	for (i = 0; i < NUM_THREADS; i++) { /* wait/join threads */
+		pthread_join(tid[i], NULL);
+	}
+	return 0;
+}
